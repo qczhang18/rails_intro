22bd77b6953060e2ac1dc29f8f8c1eacf7b33c46
qczhang18 <aznyqjai@gmail.com>
2016-12-13 00:32:10 -0500

    restful controller user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,7 @@ class UsersController < ApplicationController
   def destroy
     User.destroy(params[:id])
     flash[:notice] = "User account is deleted"
+    redirect_to users_path
   end
 
   def new
@@ -18,10 +19,35 @@ class UsersController < ApplicationController
   def create
     if User.create(user_params)
       flash[:notice] = "user created successfully!"
+      redirect_to users_path
     else
       flash.now[:error] = "Please fix your errors!"
       render "new"
     end
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash.now[:notice] = 'User updated successfully!'
+      render "edit"
+    else
+      flash.now[:alert] = 'Sorry there is a problem!'
+      render "edit"
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(
+      :fname,
+      :lname,
+      :email,
+      :password
+    )
+  end
 end
