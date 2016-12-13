a7c6d40a7d68695622823d99e2aab8a2f428d5db
qczhang18 <aznyqjai@gmail.com>
2016-12-13 15:45:01 -0500

    movies added

---
+++ db/migrate/20161213172023_create_movies.rb
@@ -0,0 +1,11 @@
+class CreateMovies < ActiveRecord::Migration[5.0]
+  def change
+    create_table :movies do |t|
+      t.string :name
+      t.text :description
+      t.integer :rating
+
+      t.timestamps
+    end
+  end
+end
